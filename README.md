a2e7fc8a411f73a3224505779f5adc74a8add035
Fernando Medina Corey <fmcorey@gmail.com>
2016-02-06 19:34:03 -0500

    Add config file inegration, change script name, add gitignore for config file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is a python tool for generating a random patent URL. It uses Google's patent database because it has more easily generated URLs. The USPTO can also be used to search for these patents.
+A python bot that tweets out randomized URLs to Google's patent database.
 
-In addition to generating a URL it will send a Tweet out on the account https://www.twitter.com/dailypatent
+You can follow the bot here: https://www.twitter.com/dailypatent
---
+++ daily_patent.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from ConfigParser import SafeConfigParser
+import random
+from twython import Twython
+
+r=random
+
+parser = SafeConfigParser()
+parser.read('config.ini')
+
+APP_KEY=parser.get('daily_patent','APP_KEY')
+APP_SECRET=parser.get('daily_patent','APP_SECRET')
+OAUTH_TOKEN=parser.get('daily_patent','OAUTH_TOKEN')
+OAUTH_TOKEN_SECRET=parser.get('daily_patent','OAUTH_TOKEN_SECRET')
+
+twitter = Twython(APP_KEY, APP_SECRET,
+                  OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+patent_areas = "Utility", "Design"
+
+def tweet(tweet_to_send):
+	twitter.update_status(status=tweet_to_send)
+
+def rand_pat_area():
+	area = patent_areas[(r.randrange(2))]
+	print area
+	return area
+
+def url_generators(an_area):
+	if an_area == patent_areas[0]:
+		final_url = "https://www.google.com/patents/US"+ str(r.randrange(9247000))
+		return final_url
+		print final_url
+	elif an_area == patent_areas[1]:
+		final_url = "https://www.google.com/patents/USD"+ str(r.randrange(748365))
+		return final_url
+		print final_url
+	else:
+		print "Somehow you broke me!"
+
+patent_url=url_generators(rand_pat_area())
+
+prefix_texts=["Here's a #patent you've probably never heard of: ","Will this #random #patent be a troll or the real-deal? ", "I like random #patents. What about you? ","Hey, there's a new random patent: ","How many #patent tweets does it take for you to find something you've used? ","Patents. Patents. #patents. Yup, here's another: "]
+
+random_prefix=prefix_texts[r.randrange(len(prefix_texts)-1)]
+
+random_status_update=random_prefix + patent_url
+
+tweet(random_status_update)
